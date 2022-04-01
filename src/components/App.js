1b56e1008454eeba56e953c27b952f1ddfc8b2ac
Harshraj <djharshraj@gmail.com>
2022-04-01 15:43:43 +0530

    task-completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Renderapp from "./Renderapp";
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <Renderapp name="Harshraj"/>
+    </div>
   )
 }
 
---
+++ src/components/Renderapp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../styles/App.css';
+
+const Renderapp = (props) => {
+
+
+    return(
+        <div id="root">
+            <p>I am learning React. My life is getting better.</p>
+        </div>
+    )
+}
+
+export default Renderapp;
